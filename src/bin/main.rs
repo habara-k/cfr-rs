ef1f0b9a9b22f6af2d685627fc8f2397899549f0
Keigo Habara <k.habara.aa@gmail.com>
2021-05-07 09:53:45 +0900

    refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,27 @@
 extern crate cfr_rs;
 use cfr_rs::*;
+use std::time::Instant;
+
+macro_rules! measure {
+    ( $x:expr) => {
+      {
+        let start = Instant::now();
+        let result = $x;
+        let end = start.elapsed();
+        println!("{}.{:03} [sec] elapsed.", end.as_secs(), end.subsec_nanos() / 1_000_000);
+        result
+      }
+    };
+  }
 
 fn main() {
-    let rule = Rule::from_name("kuhn").unwrap();
-    let prof = profile::uniform(&rule);
-    let step = 100000;
-    let nash_prof = cfr::calc_nash_strt(&rule, prof, step);
-    println!("nash_prof: {:?}", nash_prof);
+    measure!({
+        let rule = Rule::from_name("kuhn").unwrap();
+        let prof = profile::uniform(&rule);
+        let step = 10000;
+        let nash_prof = cfr::calc_nash_strt(&rule, prof, step);
+        println!("best_resp_to_p1: {}", solver::calc_best_resp_against_to(&rule, &Player::P1, nash_prof[&Player::P1].clone()).1);
+        println!("best_resp_to_p2: {}", solver::calc_best_resp_against_to(&rule, &Player::P2, nash_prof[&Player::P2].clone()).1);
+        visualizer::print(&rule, &nash_prof);
+    });
 }
