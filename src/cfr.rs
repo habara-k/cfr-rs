08d0b2111658f25f91769d87fb38d0281e55d2f1
Keigo Habara <k.habara.aa@gmail.com>
2021-05-17 11:34:10 +0900

    remove probcontribute

--- src/cfr.rs
+++ src/cfr.rs
@@ -1,7 +1,7 @@
 use super::{
     action::ActionId,
     node::{InformationSetId, Node, NodeId},
-    player::{Player, ProbContribute},
+    player::Player,
     rule::Rule,
     strategy::{self, Strategy},
 };
@@ -46,67 +46,110 @@ fn cfr_dfs(
     strt_sum: &mut Strategy,
     strt: &Strategy,
     node_id: &NodeId,
-    prob: ProbContribute,
+    pr1: f64,
+    pr2: f64,
+    prc: f64,
 ) -> f64 {
     match &rule.nodes[node_id] {
         Node::Terminal { value } => *value,
-        Node::NonTerminal { player, edges } => match player {
-            Player::C => edges
-                .iter()
-                .map(|(action_id, child_id)| {
-                    cfr_dfs(
-                        rule,
-                        regret_sum,
-                        strt_sum,
-                        strt,
-                        child_id,
-                        prob.prod(player, rule.transition[node_id][action_id]),
-                    ) * rule.transition[node_id][action_id]
-                })
-                .sum(),
-            _ => {
-                let info_set_id = &rule.info_set_id_by_node[node_id];
-
-                for (action, _) in edges {
-                    *strt_sum
-                        .get_mut(info_set_id)
-                        .unwrap()
-                        .get_mut(action)
-                        .unwrap() += prob.only(player) * strt[info_set_id][action];
-                }
-
-                let action_util: BTreeMap<ActionId, f64> = edges
+        Node::NonTerminal { player, edges } => {
+            if *player == Player::C {
+                return edges
                     .iter()
                     .map(|(action_id, child_id)| {
-                        (
-                            *action_id,
-                            cfr_dfs(
-                                rule,
-                                regret_sum,
-                                strt_sum,
-                                strt,
-                                child_id,
-                                prob.prod(player, strt[info_set_id][action_id]),
-                            ),
-                        )
+                        let t = &rule.transition[node_id][action_id];
+                        cfr_dfs(
+                            rule,
+                            regret_sum,
+                            strt_sum,
+                            strt,
+                            child_id,
+                            pr1,
+                            pr2,
+                            prc * t,
+                        ) * t
                     })
-                    .collect();
-
-                let ret: f64 = action_util
-                    .iter()
-                    .map(|(action_id, util)| strt[info_set_id][action_id] * util)
                     .sum();
-
-                for (action, util) in &action_util {
-                    *regret_sum
-                        .get_mut(info_set_id)
-                        .unwrap()
-                        .get_mut(action)
-                        .unwrap() += player.sign() * (util - ret) * prob.except(player);
-                }
-                ret
             }
-        },
+
+            let info_set_id = &rule.info_set_id_by_node[node_id];
+            let action_util: BTreeMap<ActionId, f64> = match player {
+                Player::P1 => {
+                    for (action, _) in edges {
+                        *strt_sum
+                            .get_mut(info_set_id)
+                            .unwrap()
+                            .get_mut(action)
+                            .unwrap() += pr1 * strt[info_set_id][action];
+                    }
+                    edges
+                        .iter()
+                        .map(|(action_id, child_id)| {
+                            (
+                                *action_id,
+                                cfr_dfs(
+                                    rule,
+                                    regret_sum,
+                                    strt_sum,
+                                    strt,
+                                    child_id,
+                                    pr1 * strt[info_set_id][action_id],
+                                    pr2,
+                                    prc,
+                                ),
+                            )
+                        })
+                        .collect()
+                }
+                Player::P2 => {
+                    for (action, _) in edges {
+                        *strt_sum
+                            .get_mut(info_set_id)
+                            .unwrap()
+                            .get_mut(action)
+                            .unwrap() += pr2 * strt[info_set_id][action];
+                    }
+                    edges
+                        .iter()
+                        .map(|(action_id, child_id)| {
+                            (
+                                *action_id,
+                                cfr_dfs(
+                                    rule,
+                                    regret_sum,
+                                    strt_sum,
+                                    strt,
+                                    child_id,
+                                    pr1,
+                                    pr2 * strt[info_set_id][action_id],
+                                    prc,
+                                ),
+                            )
+                        })
+                        .collect()
+                }
+                _ => panic!(),
+            };
+
+            let ret: f64 = action_util
+                .iter()
+                .map(|(action_id, util)| strt[info_set_id][action_id] * util)
+                .sum();
+
+            let pr_except = match player {
+                Player::P1 => pr2 * prc,
+                Player::P2 => pr1 * prc,
+                _ => panic!(),
+            };
+            for (action, util) in &action_util {
+                *regret_sum
+                    .get_mut(info_set_id)
+                    .unwrap()
+                    .get_mut(action)
+                    .unwrap() += player.sign() * (util - ret) * pr_except;
+            }
+            ret
+        }
     }
 }
 
@@ -135,7 +178,9 @@ pub fn calc_nash_strt(rule: &Rule, init_strt: Strategy, step: usize) -> Strategy
             &mut strt_sum,
             &latest_strt,
             &rule.root,
-            ProbContribute::new(),
+            1.0,
+            1.0,
+            1.0,
         );
 
         latest_strt = to_strategy(&regret_sum);
