b44b65cac4aaccd7daa1d31f5cae3eea9301f20b
Keigo Habara <k.habara.aa@gmail.com>
2021-11-22 20:26:31 +0900

    Refactor plot.py

--- plot.py
+++ plot.py
@@ -1,19 +1,22 @@
-import matplotlib.pyplot as plt
-import json
 import argparse
+import datetime
+import json
+import os
+
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--log')
+parser.add_argument("--log")
 args = parser.parse_args()
 
 data = json.loads(args.log)
 
-plt.plot(
-    list(map(int, data.keys())),
-    data.values())
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('step')
-plt.ylabel('nashconv')
+plt.plot(list(map(int, data.keys())), data.values())
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("step")
+plt.ylabel("nashconv")
 plt.grid()
-plt.show()
+now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+os.mkdir("log/{}".format(now))
+plt.savefig("log/{}/nash_conv.png".format(now))
